b5b25f7232b1d67827699f5fd46c5b7efed91b5b
Liquid_Ammo <167435248+Liquid-Ammo@users.noreply.github.com>
2024-07-09 11:29:37 +0000

    added variable support into seperate file to reduce changes

--- syntax.py
+++ syntax.py
@@ -10,29 +10,45 @@ while True:
   var_error = True
   
   if b[0] == "let":
-    if b[1][0] not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-      for i in b[1]:
+    
+    expression = ""
+    for i in b[1:]:
+      expression += i.strip()
+
+    variable,value = expression.split("=")
+    
+    if variable[0] not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+      for i in variable:
         var_error = True
         if i.isalpha() or i.isdigit() or i == "_":
           var_error = False
     else:
       var_error = True
-
-    expression = ""
     
     if not var_error:
-      for i in b[1:]:
-        expression += i.strip()
-      variable,value = expression.split("=")
-      print(value,type(value))
+      
       if (value[0] == '\"' and value[-1] == '\"') or (value[0] == '\'' and value[-1] == '\'') :
         var[variable] = str(value[1:-1])
+        
       elif value.isdigit():
-        var[variable] = int(value[1:-1])
+        var[variable] = int(value)
+
+      elif value == "True":
+        value = True
+        var[variable] = value
+        
+      elif value == "False":
+        value = False
+        var[variable] = value
+        
+      else:
+        print("Error: Invalid Value")
+        
     else:
       print("Error: Invalid Variable Name")
+  
   elif b[0] == "show" and len(b) == 2:
-    print(var[b[1]])
+    print(var[b[1]], type(var[b[1]]))
 
   else:
-    print("Syntax error")
+    print("Error: Invalid Syntax")
