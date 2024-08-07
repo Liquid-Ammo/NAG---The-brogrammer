ee190bb4622dc9758b2f1eff6a34f50ea5a5c965
Liquid_Ammo <167435248+Liquid-Ammo@users.noreply.github.com>
2024-08-07 11:06:06 +0000

    format

--- Up.py
+++ Up.py
@@ -1,118 +1,121 @@
 def create_table(name):
+    import csv
 
-  import csv
-  column_name = []
-  column_datatype = []
-  column_key = []
+    column_name = []
+    column_datatype = []
+    column_key = []
 
-  while True:
-    a = str(input("  : "))
-    a.strip()
-    b = a.split(" ")
+    while True:
+        a = str(input("  : "))
+        a.strip()
+        b = a.split(" ")
 
-    if b[0] == ";":
-      break
+        if b[0] == ";":
+            break
 
-    elif len(b) == 2:
+        elif len(b) == 2:
 
-      if b[0] not in column_name:
-        column_name.append(b[0])
-        column_datatype.append(b[1])
-        column_key.append(False)
+            if b[0] not in column_name:
+                column_name.append(b[0])
+                column_datatype.append(b[1])
+                column_key.append(False)
 
-      else:
-        print("Error: Repeated Attribute Name")
+            else:
+                print("Error: Repeated Attribute Name")
 
-    elif len(b) == 3:
+        elif len(b) == 3:
 
-      if b[2] == "primary":
-        column_key.append(True)
-        column_name.append(b[0])
-        column_datatype.append(b[1])
+            if b[2] == "primary":
+                column_key.append(True)
+                column_name.append(b[0])
+                column_datatype.append(b[1])
 
-      else:
-        print("Error: Invalid Key")
+            else:
+                print("Error: Invalid Key")
 
-    else:
-      print("Error: Invalid Syntax")
+        else:
+            print("Error: Invalid Syntax")
 
-    z = "csv_data//" + name + ".csv"
+        z = "csv_data//" + name + ".csv"
 
-  aq = open(z, "w")
-  bq = csv.writer(aq)
-  bq.writerow(column_name)
-  aq.close()
+    aq = open(z, "w")
+    bq = csv.writer(aq)
+    bq.writerow(column_name)
+    aq.close()
 
-  a = [column_name, column_datatype, column_key]
-  file = open("csv_data//csvdata.csv", "a+")
-  wri = csv.writer(file)    
-  wri.writerow([name,list(a[0]), list(a[1]), list(a[2])])
-  file.close()
-  file = open("csv_data//tables.csv", "a+")
-  wri = csv.writer(file)
-  wri.writerow([name])
-  file.close()
-
-
+    a = [column_name, column_datatype, column_key]
+    file = open("csv_data//csvdata.csv", "a+")
+    wri = csv.writer(file)
+    wri.writerow([name, list(a[0]), list(a[1]), list(a[2])])
+    file.close()
+    file = open("csv_data//tables.csv", "a+")
+    wri = csv.writer(file)
+    wri.writerow([name])
+    file.close()
 
 
-def var_assign(var , b) :
+def var_assign(var, b):
 
-  var_error = True
-  expression = ""
-
-  for i in b[1:]:
-    expression += i.strip()
-
-  variable, value = expression.split("=")
-
-  if variable[0] not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-
-    for i in variable:
-      var_error = True
-
-      if i.isalpha() or i.isdigit() or i == "_":
-        var_error = False
-  else:
     var_error = True
+    expression = ""
 
-  if not var_error:
-    
-    valuex= value[1::]
-    
-    if (value[0] == '\"' and value[-1] == '\"') or (value[0] == '\'' and value[-1] == '\''):
-      var[variable] = str(value[1:-1])
-      
-    elif value.isdigit():
-      var[variable] = int(value)
+    for i in b[1:]:
+        expression += i.strip()
 
-    elif value[0] == "-" and valuex.isdigit():
-      var[variable] = int(value)
+    variable, value = expression.split("=")
 
-    elif value[0] == "-" and ( valuex.split(".")[0].isdigit() and valuex.split(".")[1].isdigit() ):
-      var[variable] = float(value)
+    if variable[0] not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
 
-    elif value == "True":
-      value = True
-      var[variable] = value
+        for i in variable:
+            var_error = True
 
-    elif value == "False":
-      value = False
-      var[variable] = value
+            if i.isalpha() or i.isdigit() or i == "_":
+                var_error = False
+    else:
+        var_error = True
 
-    elif value.split(".")[0].isdigit() and value.split(".")[1].isdigit():
-      var[variable] = float(value)
+    if not var_error:
+
+        valuex = value[1::]
+
+        if (value[0] == '"' and value[-1] == '"') or (
+            value[0] == "'" and value[-1] == "'"
+        ):
+            var[variable] = str(value[1:-1])
+
+        elif value.isdigit():
+            var[variable] = int(value)
+
+        elif value[0] == "-" and valuex.isdigit():
+            var[variable] = int(value)
+
+        elif value[0] == "-" and (
+            valuex.split(".")[0].isdigit() and valuex.split(".")[1].isdigit()
+        ):
+            var[variable] = float(value)
+
+        elif value == "True":
+            value = True
+            var[variable] = value
+
+        elif value == "False":
+            value = False
+            var[variable] = value
+
+        elif value.split(".")[0].isdigit() and value.split(".")[1].isdigit():
+            var[variable] = float(value)
+
+        else:
+            print("Error: Invalid Value")
 
     else:
-      print("Error: Invalid Value")
-
-  else:
-    print("Error: Invalid Variable Name")
+        print("Error: Invalid Variable Name")
 
 
 def show_tables():
-  import csv
-  a = open ( "csv_data//tables.csv" , "r" )
-  wri = csv.reader(a)
-  for i in wri:
-    print(i[0])
+    import csv
+
+    a = open("csv_data//tables.csv", "r")
+    wri = csv.reader(a)
+    for i in wri:
+        print(i[0])
--- syntax.py
+++ syntax.py
@@ -1,6 +1,6 @@
-from Up import create_table , var_assign , show_tables
+from Up import create_table, var_assign, show_tables
 
-import os 
+import os
 
 print("NAG - The Brogramming Language  |  Interactive Mode ")
 
@@ -8,43 +8,42 @@ var = {}
 
 while True:
 
-  a = str(input(">>> "))
-  a = a.strip()
-  b = a.split(" ")
+    a = str(input(">>> "))
+    a = a.strip()
+    b = a.split(" ")
 
-  if b[0] == "let":
-    var_assign(var , b)    
+    if b[0] == "let":
+        var_assign(var, b)
 
-  elif b[0] == "show" and len(b) == 2:
+    elif b[0] == "show" and len(b) == 2:
 
-    if b[1] in var:
-      print( var[b[1]] , type(var[b[1]]) )
-    
+        if b[1] in var:
+            print(var[b[1]], type(var[b[1]]))
+
+        else:
+            print("Variable not found")
+
+    elif b[0] == "list" and b[1] == "tables" and len(b) == 2:
+        show_tables()
+
+    elif b[0] == "create" and b[1] == "table" and len(b) == 3:
+        create_table(b[2])
+
+    elif b[0] == "list" and b[1] == "var":
+
+        if len(var) == 0:
+            print(">", "Empty")
+
+        else:
+            for i in var:
+                print(">", i, " : ", var[i], type(var[i]))
+
+    elif b[0] == "clear" and len(b) == 1:
+        os.system("clear")
+        print("NAG - The Brogramming Language  |  Interactive Mode ")
+
+    elif b[0] == "var" and b[1] == "clear" and len(b) == 2:
+        var = {}
+
     else:
-      print("Variable not found")
-
-
-  elif b[0] == "show" and b[1] == "tables" and len(b) == 2:
-    show_tables()
-
-  elif b[0] == "create" and b[1] == "table" and len(b) == 3:
-    create_table(b[2])
-
-  elif b[0] == "list" and b[1] == "var":
-    
-    if len(var) == 0:
-      print(">","Empty")
-    
-    else:
-      for i in var:
-        print( ">", i, " : ", var[i] , type(var[i]))
-  
-  elif b[0] == "clear" and len(b) == 1:
-    os.system('clear')
-    print("NAG - The Brogramming Language  |  Interactive Mode ")
-
-  elif b[0]== "var" and b[1]=="clear" and len(b) == 2:
-    var={}
-
-  else:
-    print("Error: Invalid Syntax") 
+        print("Error: Invalid Syntax")
